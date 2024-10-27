b34730900d9650c4a9919257fb660efec43ba540
Phuong Phan <phuongphan.work@gmail.com>
2024-10-27 21:21:22 +0100

    Oppdatering: nå er det mulig å søke etter og fjerne maskiner fra kommandolinjen.

--- src/treningsmaskiner.java
+++ src/treningsmaskiner.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class treningsmaskiner {
     public static void main(String[] args){
@@ -6,8 +7,7 @@ public class treningsmaskiner {
         //ny arrayliste 
         ArrayList<String> listeOverMaskiner = new ArrayList<String>();
 
-        //for bein & rumpe
-        //legger til nye med add()
+        //legger til nye med add() eller i kommandolinje
         listeOverMaskiner.add("Belt squat (ATX)");
         listeOverMaskiner.add("Glute drive (Nautilus)");
         listeOverMaskiner.add("Hybrid inner/outer thigh (Prime)");
@@ -31,6 +31,75 @@ public class treningsmaskiner {
         listeOverMaskiner.add("Beinpress (Arsenal)");
         listeOverMaskiner.add("Sumo monolift (TTEKA)");
 
+        Scanner scanner = new Scanner(System.in);
+        String kommando;
+
+        while(true){
+            System.out.println("\nDette er et program med oversikt over alle treningsmaskiner på Pumphouse. Velg en handling: ");
+            System.out.println("1. Se maskiner");
+            System.out.println("2. Legg til en maskin");
+            System.out.println("3. Fjern en maskin");
+            System.out.println("4. Søk etter en maskin");
+            System.out.println("5. Avslutt program");
+
+            System.out.print("Skriv inn et valg (1-5): ");
+            kommando = scanner.nextLine();
+
+            switch(kommando){
+                case "1":
+                    //se maskiner
+                    System.out.println("\nMaskiner på Pumphouse: ");
+                    for(int i=0; i<listeOverMaskiner.size(); i++){
+                        System.out.println((i+1) + ". " + listeOverMaskiner.get(i));
+                    }
+                    break;
+                case "2":
+                    // Legg til en maskin
+                    System.out.println("Skriv inn navnet på maskinen du vil legge til: ");
+                    String nyMaskin = scanner.nextLine();
+                    listeOverMaskiner.add(nyMaskin);
+                    System.out.println("Maskinen \"" + nyMaskin + "\" er lagt til.");
+                    break;
+                case "3":
+                    // Fjern en maskin
+                    System.out.print("Skriv inn nummeret på maskinen du vil fjerne: ");
+                    int index = Integer.parseInt(scanner.nextLine()) - 1;
+                    if (index >= 0 && index < listeOverMaskiner.size()) {
+                        String fjernetMaskin = listeOverMaskiner.remove(index);
+                        System.out.println("Maskinen \"" + fjernetMaskin + "\" er fjernet. ");
+                    }
+                    else {
+                        System.out.println("Ugyldig nummer!");
+                    }
+                    break;
+                case "4":
+                    // Søk etter en maskin
+                    System.out.println("Skriv inn søkeord for maskinen: ");
+                    String sok = scanner.nextLine().toLowerCase();
+                    System.out.println("\nSøkeresultater: ");
+                    boolean funnetSokeord = false;
+                    for (String maskin : listeOverMaskiner){
+                        if(maskin.toLowerCase().contains(sok)){
+                            System.out.println(maskin);
+                            funnetSokeord = true;
+                        }
+                    }
+                    if(!funnetSokeord){
+                        System.out.println("Ingen maskiner funnet med søkeordet: " + sok);
+                    }
+                    break;
+                case "5":
+                    // Avslutt programmet
+                    System.out.println("Avslutter programmet.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Ugyldig valg, prøv igjen.");
+            }
+        }
+
+        
+        /* TESTER
 
         //print ut antall med size()
         System.out.println("Pumhouse har " + listeOverMaskiner.size() + " beinmaskiner akkurat nå.");
@@ -49,7 +118,7 @@ public class treningsmaskiner {
         System.out.println("Index til Hack Squat Cybex er: " + indexTilMaskin);
 
 
-
+        */
 
     
    
